c7234039f11164da065ec1042b5912659dc5c313
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-04 17:54:32 +0900

    RESAS API の「都道府県一覧」から API を取得する

---
+++ components/AppPrefectures.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul v-for="prefecture in prefectures" :key="prefecture.prefCode">
+      <li>{{ prefecture.prefName }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prefectures: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,34 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        yumemi-codecheck
-      </h1>
-      <h2 class="subtitle">
-        My neat Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <AppPrefectures :prefectures="prefectures" />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AppPrefectures from '~/components/AppPrefectures.vue';
 
 export default {
   components: {
-    Logo
+    AppPrefectures
+  },
+  data() {
+    return {
+      prefectures: []
+    };
+  },
+  async asyncData({ app }) {
+    const { result } = await app.$axios
+      .$get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: {
+          'X-API-KEY': process.env.API_KEY
+        }
+      });
+
+    return { prefectures: result };
   }
-}
+};
 </script>
 
 <style>
@@ -46,27 +39,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
