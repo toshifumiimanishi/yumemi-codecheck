a346728150b1e42dff870834031407554de29c15
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-04 19:17:02 +0900

    RESAS API から選択された都道府県の「人口構成」を取得する

--- components/AppPrefectures.vue
+++ components/AppPrefectures.vue
@@ -5,7 +5,7 @@
       <ul class="prefecture_list">
         <li class="prefecture_item" v-for="prefecture in prefectures" :key="prefecture.prefCode">
           <label>
-            <input type="checkbox" :value="prefecture.prefName" v-model="checkedPrefName">
+            <input type="checkbox" :value="prefecture.prefCode" v-model="checkedPrefCode" @change="onCheckedPrefName">
             {{ prefecture.prefName }}
           </label>
         </li>
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   props: {
     prefectures: {
@@ -23,7 +25,43 @@ export default {
   },
   data() {
     return {
-      checkedPrefName: []
+      checkedPrefCode: [],
+      totalPopulation: []
+    }
+  },
+
+  methods: {
+    async fetchPopulationComposition(prefCode) {
+      const { data: { result } } = await axios.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`, {
+        headers: {
+         'X-API-KEY': process.env.API_KEY
+        }
+      });
+      const totalPopulation = result.data[0];
+
+      this.totalPopulation = [...this.totalPopulation, {
+        prefCode,
+        data: totalPopulation.data
+      }];
+    },
+
+    removeTotalPopulation(prefCode) {
+      const index = this.totalPopulation.findIndex(population => {
+        return population.prefCode === prefCode;
+      });
+
+      this.totalPopulation.splice(index, 1);
+    },
+
+    onCheckedPrefName(event) {
+      const prefCode = event.currentTarget.value;
+      const isChecked = event.currentTarget.checked;
+
+      if (isChecked) {
+        this.fetchPopulationComposition(prefCode);
+      } else {
+        this.removeTotalPopulation(prefCode);
+      }
     }
   }
 }
