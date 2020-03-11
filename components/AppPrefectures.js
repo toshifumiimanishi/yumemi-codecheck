41159f87d3c3566d27636a3b8132e409e5b6c648
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-11 21:47:00 +0900

    グローバル CSS（Sass）の作成 + Built-In Sass での読み込み

---
+++ components/AppPrefectures.js
@@ -0,0 +1,16 @@
+const AppPrefectures = () => (
+  <div className="prefecture">
+    <div className="prefecture_container">
+      <h2 className="prefecture_heading">都道府県</h2>
+      <ul className="prefecture_list">
+        <li className="prefecture_item">
+          <label>
+            <input type="checkbox" />
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default AppPrefectures;
