428182ffc5729a07c708ad3b5507f27f7d44e1f4
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-06-11 13:38:11 +0900

    都道府県の一覧コンポーネントの読み込み

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PrefecturesModule } from './prefectures/prefectures.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrefecturesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
